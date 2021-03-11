e8e4f303542e414878191d1b28f4ea12c5a065aa
Renee Thomsen <rcthomsen19@gmail.com>
2021-03-11 13:18:19 -0500

    started udacimealsapp

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name udacimealsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the udacimealsApp
+ */
+angular.module('udacimealsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
